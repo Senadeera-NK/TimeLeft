a7c928c205ecdce4c69bc0de4eb6dc8db77d0c8d
N.K. Senadeera <66235626+Senadeera-NK@users.noreply.github.com>
2025-03-24 05:48:15 +0000

    just main files

---
+++ countdown-timer/src/CountDownTimer.js
@@ -0,0 +1,10 @@
+const CountDownTimer = () => {
+    const targetDate = new Date("2026-03-06T00:00:00").getTime();
+    return(
+        <div>
+            <h1>Begining</h1>
+        </div>
+    );
+};
+
+export default CountDownTimer;
